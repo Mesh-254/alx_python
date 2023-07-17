53b28c8e8ac8d477703de05c68a260a93a409c83
mesh-254 <meshack1995@gmail.com>
2023-07-17 10:58:30 +0300

    program to print last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import random
+import math
 number = random.randint(-10000, 10000)
 
 # get the last digit of a positive integer using modulo operator (%).
-remainder = number % 10
-if remainder > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5"
-          .format(number, remainder), end="\n")
-elif remainder == 0:
-    print("Last digit of {:d} is {:d} and is 0"
-          .format(number, remainder), end="\n")
-else:
+if number < 0:
+    remainder = number % -10
     print("Last digit of {:d} is {:d} and is less than 6 and not 0"
           .format(number, remainder), end="\n")
+elif number > 0:
+    remainder = number % 10
+    if remainder > 5:
+        print("Last digit of {:d} is {:d} and is greater than 5"
+              .format(number, remainder), end="\n")
+    elif remainder == 0:
+        print("Last digit of {:d} is {:d} and is 0"
+              .format(number, remainder), end="\n")
---
+++ python-hello_world/three.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import math
+import random
+number = -1009
+
+rem = math.fmod()
+
+print(rem)
