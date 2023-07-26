7b97a3b0b19b75f4e4e7235e8cf712dfb7d152e1
mesh-254 <meshack1995@gmail.com>
2023-07-26 11:17:11 +0300

    imports the variable a from the file variable_load_2.py

---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from variable_load_2 import a
+
+
+# function to print imported value
+def print_value():
+    print(a)
+
+
+if __name__ == __name__:
+    print_value()
+    # calling the defined method inside if statement.
