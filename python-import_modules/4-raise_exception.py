6a08cb3b7eaebe359a3a0d18cb32f04bf8006f3d
mesh-254 <meshack1995@gmail.com>
2023-07-26 11:46:31 +0300

    unction that raises a type exception.

---
+++ python-import_modules/4-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception():
+    """Function that raises a type exception.
+    """
+    try:
+        raise (TypeError)
+    except TypeError:
+        raise TypeError
