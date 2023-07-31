a14befa1b6b3b8ff74bb6bfafa3eb483864979e9
mesh-254 <meshack1995@gmail.com>
2023-07-31 23:16:11 +0300

    class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 class Square:
     """
-    A square class that defines square 
-
-    args: Private instance attribute: size
-
-    Instantiation with size (no type/value verification)
+    A square class that defines square
+    atttributes:
+        Private instance attribute: size
+    returns:
+        private attribute size
     """
 
     def __init__(self, size):
-        """Private instance size initialization """
+        """
+        Private instance size initialization
+        attributes:
+            int value of the side length for a square object.
+        returns:
+            private attribute size
+        """
         self.__size = size
