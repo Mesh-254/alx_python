f32195370350fdb30950134093844637b07ca1f3
mesh-254 <meshack1995@gmail.com>
2023-07-31 23:08:54 +0300

    class Square that defines a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A square class that defines square 
+
+    args: Private instance attribute: size
+
+    Instantiation with size (no type/value verification)
+    """
+
+    def __init__(self, size):
+        self.__size = size
---
+++ python-classes/README.md
@@ -0,0 +1,8 @@
+# Python - Classes and Objects
+
+## 0. Square with size
+### Write a class Square that defines a square by:
+
+- Private instance attribute: size
+- Instantiation with size (no type/value verification)
+- You are not allowed to import any module
