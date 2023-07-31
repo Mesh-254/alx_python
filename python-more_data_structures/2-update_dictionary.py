4bb4adcfbf9d63d84a7512b574dad86ec6f960e9
mesh-254 <meshack1995@gmail.com>
2023-07-31 18:44:11 +0300

    Update dictionary to add or replace key, value

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -5,4 +5,6 @@ def update_dictionary(a_dictionary, key, value):
     """
     if key in a_dictionary:
         a_dictionary[key] = value
+    elif key not in a_dictionary:
+        a_dictionary[key] = value
     return a_dictionary
---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """returns a key with the 
+       biggest integer value
+    """
+    largest_value = a_dictionary.get
+    for k in a_dictionary:
+        if  > largest_value:
+            largest_value = k
+    return largest_value
