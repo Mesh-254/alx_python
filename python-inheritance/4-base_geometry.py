f75a748e44119abc8db9fec2af48a3bc25d56e09
mesh-254 <meshack1995@gmail.com>
2023-08-01 17:51:18 +0300

    created Public instance method that raises exception

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """BaseGeometry module"""
+
+
 class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
