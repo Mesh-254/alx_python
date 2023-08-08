12d41403ccef57fd73f299e2b10cc12b3e3af7e4
mesh-254 <meshack1995@gmail.com>
2023-08-08 16:19:44 +0300

    class Square that inherits from Rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square models"""
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """class constructor"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """informal string representation of the square"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
+                                                         self.y, self.width)
