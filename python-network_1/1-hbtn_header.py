c5f945643466df30730b6b4a45135efd69f5ea61
mesh-254 <meshack1995@gmail.com>
2023-08-09 09:56:57 +0300

    displays the value of the variable X-Request-Id in the response header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""response header module"""
+"""
+takes in a URL, sends  a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the
+response
+"""
 import requests
 import sys
 
