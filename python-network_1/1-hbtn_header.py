2e2610745074b35dd1a16ef4c477fdaf9ed33336
mesh-254 <meshack1995@gmail.com>
2023-08-09 10:04:07 +0300

    displays the value of the variable X-Request-Id in the response header

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
-takes in a URL, sends  a request to the URL and displays the
-value of the X-Request-Id variable found in the header of the
-response
-"""
+"""Header module"""
 import requests
 import sys
 
 url = sys.argv[1]
 
 if __name__ == "__main__":
-    """takes in a URL, sends a request to the URL and displays
+    """
+    takes in a URL, sends a request to the URL and displays
     the value of the X-Request-Id variable found in the header
-    of the response"""
-    
+    of the response
+    """
 
     response = requests.get(url)
 
-    # request_header_id = response.headers['X-Request-Id']
-    print(response.headers['X-Request-Id'])
+    request_header_id = response.headers['X-Request-Id']
+    print(request_header_id)
