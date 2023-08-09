b85a7e7ef66b27f838de37885b2fb67e39473937
mesh-254 <meshack1995@gmail.com>
2023-08-09 15:48:44 +0300

    script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -5,8 +5,13 @@ import sys
 import requests
 
 if __name__ == '__main__':
+    """script that takes in a URL and 
+        an email address, sends a POST request 
+        to the passed URL with the email as
+        a parameter
+    """
     url = sys.argv[1]
-    email = {"email":sys.argv[2]}
+    email = {"email": sys.argv[2]}
 
     response = requests.post(url, data=email)
 
