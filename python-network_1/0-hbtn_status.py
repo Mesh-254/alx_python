c9fd063ba5aacae85ba1af60d4f36e26cb4bc817
mesh-254 <meshack1995@gmail.com>
2023-08-09 06:13:50 +0300

    Python script that fetches

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,4 +2,9 @@
 """Making request module"""
 import requests
 
-request = requests.get("https://alu-intranet.hbtn.io/status")
+response = requests.get('https://alu-intranet.hbtn.io/status')
+
+print(dir(response))
+print("Body response:")
+print("\t- type: {}".format(type(response.text)))
+print("\t- content: {}".format(response.text))
