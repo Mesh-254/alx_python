1663d68f2d4d9d6016f660a7be0efe8ede2e5ca4
Mesh-254 <meshack1995@gmail.com>
2023-08-29 14:59:08 +0300

    script that starts a Flask web application and renders page

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 
-from flask import Flask, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,13 +14,13 @@ def index():
 
 @app.route('/hbnb', strict_slashes=False)
 def show_hbnb():
-    """function that return home route"""
+    """function that return hbnb route"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """function that return home route"""
+    """function that return c text route"""
     text = text.replace('_', ' ')
     return f'C {text}'
 
@@ -28,15 +28,22 @@ def c_text(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    """function that return home route"""
+    """function that return python text route"""
     text = text.replace('_', ' ')
     return f'Python {text}'
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def display_integer(n):
-    """function that return home route"""
+    """function that return display integer route"""
     return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_page(n):
+    """function that return display page"""
+
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
