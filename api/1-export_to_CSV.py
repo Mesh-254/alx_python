dac1211fa10c24e88339cd65d7b3108f94675fa7
Mesh-254 <meshack1995@gmail.com>
2023-10-04 09:00:11 +0300

    Python script to export data in the CSV format.

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+
 """
 This script gathers data from an API
 and displays information about a specific
-employee's tasks."""
+employee's tasks.
+"""
 
 # Import the necessary libraries
 from sys import argv
 import csv
+import json
 import requests
-
 
 # Get the user_id from the command line arguments
 user_id = argv[1]
@@ -17,7 +19,7 @@ user_id = argv[1]
 url_todos = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
 
 # Define the endpoint URL to get specific employee details
-url_user = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+url_user = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
 # Make an API request to retrieve user data
 user_data = requests.get(url_user)
@@ -28,21 +30,26 @@ username = res['username']
 todos_response = requests.get(url_todos)
 todo_data = todos_response.json()
 
-# Initialize lists
+# Initialize an empty list to store rows
 csv_rows = []
 
-
-# Iterate through the todo items and count completed and not completed tasks
+# Iterate through the todo items and build rows for the CSV
 for x in todo_data:
+    task_id = str(x['id'])
     title = x['title']
-    taskStatus = x["completed"]
+    taskStatus = str(x["completed"])
 
-    # Format the row as a tuple
-    row = (user_id, username, taskStatus, title)
+    # Format the row as a string
+    row = f'"{user_id}","{username}","{taskStatus}","{title}"'
+
+    # Append the formatted row to csv_rows
     csv_rows.append(row)
-cvs_file = f'{user_id}.csv'
-with open(cvs_file, 'w', encoding='utf-8', newline='') as f:
-    writer = csv.writer(f)
 
+# Define the CSV file name
+csv_file = f'{user_id}.csv'
+
+# Write the contents to the CSV file
+with open(csv_file, 'w', encoding='utf-8', newline='') as f:
     # Write the data rows
-    writer.writerows(csv_rows)
+    for row in csv_rows:
+        f.write(row + '\n')
