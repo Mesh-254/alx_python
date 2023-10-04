70b6e38e7a5ceb519eb8308459f202ec863f0127
Mesh-254 <meshack1995@gmail.com>
2023-10-04 18:30:50 +0300

    Python script to export data in the CSV format.

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -45,11 +45,11 @@ for x in todo_data:
     # Append the formatted row to csv_rows
     csv_rows.append(row)
 
-
-
+# Define the CSV file name
+csv_file = f'{str(user_id)}.csv'
 
 # Write the contents to the CSV file
-with open(str(user_id) + ".csv", 'w', encoding='utf-8', newline='') as f:
+with open(csv_file, 'w', encoding='utf-8', newline='') as f:
     # Write the data rows
     for row in csv_rows:
         f.write(row + '\n')
